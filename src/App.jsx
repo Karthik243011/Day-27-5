c01535c366b53076292485f1e767a82965afc16d
Karthik243011 <karthick.kutty30@gmail.com>
2025-06-03 14:52:45 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import data from "./Components/prd.json"
+import "./Components/style.css"
+
+function App(){
+    return(
+        <div>
+            <h1>Product Inventory</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Sl.no</th>
+                        <th>Name</th>
+                        <th>Category</th>
+                        <th>Price</th>
+                        <th>In Stock</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((a) => {
+                            return(
+                                <tr>
+                                    <td>{a.id}</td>
+                                    <td>{a.name}</td>
+                                    <td>{a.category}</td>
+                                    <td>{a.price}</td>
+                                    <td>{a.price}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default App;
